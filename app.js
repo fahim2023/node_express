42eca06d447725e0618eab392ad8a68901e43de4
fahim2023 <fa733m4d4m@outlook.com>
2026-01-12 21:29:57 +0000

    add basic styling to html pages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const rootDir = require("./util/path");
+
 const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -11,6 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 app.listen(3000);
